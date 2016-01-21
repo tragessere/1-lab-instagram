fc8f9e60d2c8490c62f0dc57a617fd025eb30e88
Darrell Shi <darrell_authentic@yahoo.com>
2016-01-21 16:40:38 -0500

    added table view

--- Instagram Feed/PhotosViewController.swift
+++ Instagram Feed/PhotosViewController.swift
@@ -7,44 +7,71 @@
 //
 
 import UIKit
+import AFNetworking
 
 class PhotosViewController: UIViewController {
-  
-  var instagramData: [NSDictionary]?
-
+    
+    @IBOutlet weak var instagramTableView: UITableView!
+    var instagramData: [NSDictionary]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-      loadData()
+        
+        instagramTableView.delegate = self
+        instagramTableView.dataSource = self
+        
+        instagramTableView.rowHeight = 320
+        
+        loadData()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-  func loadData() {
-    let clientId = "e05c462ebd86446ea48a5af73769b602"
-    let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
-    let request = NSURLRequest(URL: url!)
-    let session = NSURLSession(
-      configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
-      delegate:nil,
-      delegateQueue:NSOperationQueue.mainQueue()
-    )
     
-    let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
-      completionHandler: { (dataOrNil, response, error) in
-        if let data = dataOrNil {
-          if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
-            data, options:[]) as? NSDictionary {
-              NSLog("response: \(responseDictionary)")
-              
-              self.instagramData = responseDictionary.valueForKey("data") as? [NSDictionary]
-          }
+    func loadData() {
+        let clientId = "e05c462ebd86446ea48a5af73769b602"
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+//                            NSLog("response: \(responseDictionary)")
+                            
+                            self.instagramData = responseDictionary.valueForKey("data") as? [NSDictionary]
+                            self.instagramTableView.reloadData()
+                    }
+                }
+        });
+        task.resume()
+    }
+}
+
+extension PhotosViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let instagramData = instagramData {
+            return instagramData.count
         }
-    });
-    task.resume()
-  }
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = instagramTableView.dequeueReusableCellWithIdentifier("postCell", forIndexPath: indexPath) as! PostCell
+        
+        let post = instagramData![indexPath.row] as NSDictionary
+        let imageUrl = ((post["images"] as! NSDictionary)["standard_resolution"] as! NSDictionary)["url"] as! String
+        
+        cell.postImageView.setImageWithURL(NSURL(string: imageUrl)!)
+        return cell
+    }
+    
 }
 
---
+++ Instagram Feed/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  Instagram Feed
+//
+//  Created by Darrell Shi on 1/21/16.
+//  Copyright © 2016 iOS Development. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var postImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
